4351d3e2a2ecfdee06968e8c9ea613247dcadaf9
MafijulAlam <mafijulcse@gmail.com>
2022-02-25 01:04:11 +0600

    thard

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -8,6 +8,8 @@ import Checkout from './Checkout';
 const Cart = (props) => {
 
   const [isOrder, setIsOrder] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [didSubmit, setDidSubmit] = useState(false);
 
   const cartCtx = useContext(CartContext)
 
@@ -20,9 +22,25 @@ const Cart = (props) => {
       cartCtx.addItem({...item, amount:1})
     }
 
+    const submitOrderHandler = async (userData)=>{
+      setIsSubmitting(true)
+      await fetch ('https://online-food-a4293-default-rtdb.firebaseio.com/orders.json',{
+        method:'POST',
+        body: JSON.stringify({
+          user: userData,
+          orderedItems: cartCtx.items
+
+        })
+      })
+      setIsSubmitting(false)
+      setDidSubmit(true)
+      cartCtx.clearCart()
+    }
+
     const cartItems = ( 
       <ul className={styles['cart-items']}>
         {cartCtx.items.map( (item) => (
+
 
           <CartItem 
             key={item.id} 
@@ -43,7 +61,24 @@ const Cart = (props) => {
     <button className={styles['button--alt']} onClick={props.onClose} >Close</button>
     <button className={styles.button} onClick={orderHandler}>Order</button>
     </div>
+
+    const  cartModalContent = <> {cartItems}
+        <div className={styles.total}>
+          <span>Total Amount</span>
+          <span> TK:{totolAmount} </span>
+      </div>
+      {isOrder && <Checkout onConfirm={submitOrderHandler} onCancel={props.onClose} /> }
+      {!isOrder && orderdiv}
+    </>
     
+    const isSubmittingModalContent = <p>Sending order data...</p>
+    const didSubmitModalContent =<>
+        <p> Successfully sent the order</p>
+        <div className={styles.actions}>
+    <button className={styles.button} onClick={props.onClose} >Close</button>
+    </div>
+      </> 
+
     return (
         <>
            {/* <Modal onClose={props.onClose}>
@@ -61,14 +96,10 @@ const Cart = (props) => {
 
 
             <Modal onClose={props.onClose}>
-                 {cartItems}
-                   <div className={styles.total}>
-                     <span>Total Amount</span>
-                     <span> TK:{totolAmount} </span>
-                  </div>
-                  {isOrder && <Checkout onCancel={props.onClose} /> }
-                  {!isOrder && orderdiv}
-                   
+                 
+                 {!isSubmitting && !didSubmit && cartModalContent}  
+                 {isSubmitting && isSubmittingModalContent}
+                 {!isSubmitting && didSubmit && didSubmitModalContent}
                 </Modal>
         </>
     )
